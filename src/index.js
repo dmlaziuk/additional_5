db92725b01b94b918fd0648e75135ecf5382e56b
dmlaziuk <dm.laziuk@gmail.com>
2018-09-16 22:42:32 +0300

    Implement check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = bracketsConfig.map(pair => pair.join(''));
+  const trim = (string) => {
+    let result = string;
+    brackets.forEach((bracket) => { result = result.replace(bracket, ''); });
+    if (result.length === 0) return true;
+    if (string.length === result.length) return false;
+    return trim(result, brackets);
+  };
+
+  return trim(str, brackets);
+};
